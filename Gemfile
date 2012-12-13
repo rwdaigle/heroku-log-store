d4323e84f4e2fca68d4ee669f90f4a2c15c82877
Ryan Daigle <ryan.daigle@gmail.com>
2012-12-13 16:26:29 -0500

    Basic pg connection

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 ruby "1.9.3"
 
 gem 'goliath'
+gem 'sequel'
+gem 'pg'
--- server.rb
+++ server.rb
@@ -1,7 +1,29 @@
-require 'goliath'
+require 'bundler/setup'
+Bundler.require
+
+DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/clorox")
 
 class Hello < Goliath::API
   def response(env)
-    [200, {}, "Hello World"]
+    [200, {}, DB[:events]]
   end
 end
+
+__END__
+
+CREATE EXTENSION hstore;
+
+CREATE table events (
+  id SERIAL8 PRIMARY KEY,
+  emitted_at TIMESTAMP WITH TIME ZONE,
+  received_at TIMESTAMP WITH TIME ZONE,
+  data HSTORE
+);
+
+CREATE INDEX events_emitted_at ON events(emitted_at);
+CREATE INDEX events_received_at ON events(received_at);
+CREATE INDEX events_data ON events USING GIN(data);
+
+https://github.com/jzimmek/em-postgresql-sequel
+https://github.com/levicook/goliath-postgres-spike
+https://github.com/jtoy/em-postgres
