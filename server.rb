883cd250b8f8f88bbacc25f9ec7f0aa84a23f920
Ryan Daigle <ryan.daigle@gmail.com>
2012-12-13 21:33:50 -0500

    Crudely yanked syslog parsing from l2met

--- server.rb
+++ server.rb
@@ -1,15 +1,68 @@
 require 'bundler/setup'
 Bundler.require
+STDOUT.sync = true
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/clorox",
   :max_connections => ENV['MAX_DB_CONNECTIONS'] ? ENV['MAX_DB_CONNECTIONS'].to_i : 4
 )
 DB.extension :pg_hstore
 
-class Hello < Goliath::API
+class Parsley
+
+  LineRe = /\<\d+\>1 (\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\+00:00) [a-z0-9-]+ ([a-z0-9\-\_\.]+) ([a-z0-9\-\_\.]+) \- (.*)$/
+
+  attr_reader :data
+  attr_accessor :format
+
+  def initialize(data, format = :heroku)
+    self.format = format
+    @data = data
+  end
+
+  # frame <prority>version time hostname <appname-missing> procid msgid [structured data] msg
+  # 120 <40>1 2012-11-30T06:45:29+00:00 heroku web.3 d.73ea7440-270a-435a-a0ea-adf50b4e5f5a - State changed from starting to up
+  def lines(&block)
+    d = data
+    while d && d.length > 0
+      if matching = d.match(/^(\d+) /) # if have a counting frame, use it
+        num_bytes = matching[1].to_i
+        frame_offset = matching[0].length
+        line_end = frame_offset + num_bytes
+        msg = data[frame_offset..line_end]
+        yield msg
+        d = d[line_end..d.length]
+      elsif matching = d.match(/\n/) # Newlines = explicit message delimiter
+        d = matching.post_match
+      else
+        STDERR.puts("Unable to parse: #{d}")
+      end
+    end
+  end
+
+  def events(&block)
+    lines do |line|
+      if(matching = line.match(LineRe))
+        yield Time.parse(matching[1]).utc, matching[2], matching[3], matching[4]
+      end
+    end
+  end
+end
+
+class CloroxServer < Goliath::API
+
   def response(env)
-    DB[:events].insert([:emitted_at, :received_at, :data], [Time.now.utc, Time.now.utc, {'key' => 'value', 'at' => 'event-start'}.hstore])
-    [200, {}, DB[:events].order(:id).reverse.limit(25).collect { |r| r[:id] }.join(', ')]
+
+    if(env[Goliath::Request::REQUEST_METHOD] == 'POST')
+      parsley = Parsley.new(env[Goliath::Request::RACK_INPUT].read)
+      parsley.events do |emitted_at, process, drain_token, data|
+        STDOUT.puts(emitted_at.to_s + " -- " + data)
+      end
+    end
+
+    # DB[:events].insert([:emitted_at, :received_at, :data], [Time.now.utc, Time.now.utc, {'key' => 'value', 'at' => 'event-start'}.hstore])
+    # [200, {}, DB[:events].order(:id).reverse.limit(25).collect { |r| r[:id] }.join(', ')]
+
+    [200, {}, "ok"]
   end
 end
 
